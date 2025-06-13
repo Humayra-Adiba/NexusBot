0c79b235e05de4a8eb105b1aae4153c5f06ef25f
Humayra-Adiba <tahsinadiba2024@outlook.com>
2025-06-13 13:40:58 +0600

    Implement Compliment, Emoji, and Roast commands with random responses

---
+++ slash/slashfun/emoji.py
@@ -0,0 +1,25 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord import Interaction
+import random
+
+class EmojiCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @nextcord.slash_command(name="emoji", description="Get a random emoji")
+    async def emoji(self, interaction: Interaction):
+        emojis = ["😀", "😂", "😍", "🥳", "😎", "😱", "🤖", "👻", "🔥", "🌈", "🎉", "✨", "💩", "👀", "☠️", "🤧", "🥰","😝","🤬"]
+        chosen = random.choice(emojis)
+
+        embed = nextcord.Embed(
+            title="🎭 Random Emoji",
+            description=f"Here’s one for you: **{chosen}**",
+            color=nextcord.Color.orange()
+        )
+        embed.set_footer(text=f"Requested by {interaction.user}", icon_url=interaction.user.display_avatar.url)
+        await interaction.response.send_message(embed=embed)
+
+def setup(bot):
+    bot.add_cog(EmojiCommand(bot))
+    print("EmojiCommand loaded successfully.")
